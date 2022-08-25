6f23d350ad1668c8b9bf3696dc51f6758e6a6685
Luan Cucato <luan-kuki@hotmail.com>
2022-08-25 15:37:07 -0300

    1ºfinal models Fusion

--- fusion/models.py
+++ fusion/models.py
@@ -41,3 +41,72 @@ class Service(models.Model):
 
      def __str__(self):
          return self.titulo
+
+
+class Resource(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    icone = models.CharField(max_length=100)
+    empresa = models.ForeignKey(Empresa,on_delete=models.CASCADE)  # relaciona o registro da tabela com o registro da tabela  Empresa
+
+    class Meta:
+        verbose_name = 'Recurso'
+        verbose_name_plural = 'Recursos'
+
+    def __str__(self):
+        return self.titulo
+
+
+class Team(models.Model):
+    nome = models.CharField(max_length=100)
+    cargo = models.TextField(blank=True, null=True)
+    descricao = models.TextField(max_length=100)
+    facebook_url = models.EmailField(blank=True, null=True)
+    twitter_url = models.EmailField(blank=True, null=True)
+    instagran_url = models.EmailField(blank=True, null=True)
+    class Meta:
+        verbose_name = 'Equipe'
+        verbose_name_plural = 'Equipes'
+
+    def __str__(self):
+        return self.nome
+
+class Plan(models.Model):
+    preco = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
+    titulo = models.TextField(max_length=100,blank=True, null=True)
+    icone = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Plano'
+        verbose_name_plural = 'Planos'
+
+    def __str__(self):
+        return self.preco
+
+
+class Speech(models.Model):
+    imagem = models.ImageField( blank=True, null=True)
+    nome = models.TextField(max_length=100,blank=True, null=True)
+    cargo = models.CharField(max_length=100)
+    descricao = models.TextField(max_length=100)
+    nota = models.DecimalField(max_length=1)
+
+    class Meta:
+        verbose_name = 'Depoimento'
+        verbose_name_plural = 'Depoimentos'
+
+    def __str__(self):
+        return self.nota
+
+class Contact(models.Model):
+    nome = models.TextField(max_length=100,blank=True, null=True)
+    email = models.EmailField(max_length=100,blank=True, null=True)
+    assunto = models.TextField(max_length=100)
+    menssagem = models.TextField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Contato'
+        verbose_name_plural = 'Contatos'
+
+    def __str__(self):
+        return self.nome
